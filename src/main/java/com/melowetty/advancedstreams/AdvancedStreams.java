4b700574c21daf790754f09be40f88c1d857e21c
Melowetty <malinin2300@mail.ru>
2020-12-25 23:54:50 +0500

    create project in idea

---
+++ src/main/java/com/melowetty/advancedstreams/AdvancedStreams.java
@@ -0,0 +1,17 @@
+package com.melowetty.advancedstreams;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AdvancedStreams extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
